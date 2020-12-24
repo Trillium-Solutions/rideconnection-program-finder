6404cca500792f92cd648d08e1c193a42dbd841f
Nome Q <dickerson.nomi@gmail.com>
2020-12-23 18:20:18 -0800

    finish last link

--- src/components/StopInfo/index.js
+++ src/components/StopInfo/index.js
@@ -17,14 +17,14 @@ const StopInfo = props => {
     let trimetURL = `https://trimet.org/ride/stops_near.html?has_geocode=false&geo_type=place&placeCoord=${formData.origin.lat}%2C${formData.origin.lng}&place=${formData.origin.name}`;
     return (
         <div class={styles.stopInfoContainer}>
-            <p>{stopText} within 0.25 miles of your starting address. <a href={trimetURL}>Get directions to the nearest bus stops on the TriMet website.</a></p>
+            <p>{stopText} within 0.25 miles of your starting address. <a target={"_blank"} rel="noopener noreferrer" href={trimetURL}>Get directions to the nearest bus stops on the TriMet website.</a></p>
             <p>If you are unsure of how to take transit, you may be eligible for the <a href="https://rideconnection.org/services/travel-training">RideWise Travel Training Program</a>.</p>
             <p>Call 503-226-0700 or TTY: 7-1-1 (Oregon Relay Service) to learn more about travel training or other services</p>
-            <p>Try these services first! <br />
-            <a target="_blank" href="https://rideconnection.org/services/additional-programs">Ride Together Mileage Reimbursement</a><br />
-            <a target="_blank" href="https://rideconnection.org/services/additional-programs">Veterans Helping Veterans</a>
+            <p>Try these services first!<br />
+            <a target={"_blank"} rel="noopener noreferrer" href="https://rideconnection.org/services/additional-programs">Ride Together Mileage Reimbursement</a><br />
+            <a target={"_blank"} rel="noopener noreferrer" href="https://rideconnection.org/services/additional-programs">Veterans Helping Veterans</a>
             <br /><hr />
-            <a target="_blank" href="https://trimet.org/lowincome/index.htm">TriMet's Reduced Fare Program</a>
+            <a target={"_blank"} rel="noopener noreferrer" href="https://trimet.org/lowincome/index.htm">TriMet's Reduced Fare Program</a>
             </p>
         </div>
     );
