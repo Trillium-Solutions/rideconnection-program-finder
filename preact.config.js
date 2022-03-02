acbf331051e53ab7fbbc9dfa7c4518ac1512a050
ed-g <ed-g@users.noreply.github.com>
2022-03-02 13:08:00 -0800

    preact flags and remove reference to ExtractTextPlugin

--- preact.config.js
+++ preact.config.js
@@ -2,8 +2,8 @@ export default (config, env, helpers) => {
   delete config.entry.polyfills;
   config.output.filename = "[name].js";
 
-  let { plugin } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
-  plugin.options.disable = true;
+  //let { plugin } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
+  //plugin.options.disable = true;
 
   if (env.production) {
     config.output.libraryTarget = "umd";
